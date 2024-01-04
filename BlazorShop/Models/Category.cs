a1a76d16a813cf0ae7ac65c75825a635674641a1
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-01-04 02:04:53 +0100

    FEAT: Iniciando Projecto

---
+++ BlazorShop/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorShop.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
---
+++ BlazorShop/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorShop.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
