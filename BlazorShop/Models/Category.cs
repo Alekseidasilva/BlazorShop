f3cec5ba9cf93c7ae8bf8d8e851c1b29a2ef430c
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-01-04 02:10:06 +0100

    FEAT: Add Data Annotation

--- BlazorShop/Models/Category.cs
+++ BlazorShop/Models/Category.cs
@@ -1,7 +1,13 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "informe o Titulo")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter no minimo 3 caracteres")]
+    [MaxLength(150, ErrorMessage = "A categoria deve ter no maximo 60 caracteres")]
     public string Title { get; set; } = string.Empty;
 }
--- BlazorShop/Models/Product.cs
+++ BlazorShop/Models/Product.cs
@@ -1,10 +1,18 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Informe o Titulo")]
+    [MinLength(3, ErrorMessage = "O Produto deve ter no minimo 3 caracteres")]
+    [MaxLength(150, ErrorMessage = "o Produto deve ter no maximo 60 caracteres")]
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Infome o preço do Produto")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
 
 
